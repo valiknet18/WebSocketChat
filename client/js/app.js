489f6820c513b1af902c6ead9a3c154940a26253
Valik Grinevich <valik.v1per@gmail.com>
2015-08-15 00:48:31 +0300

    make some update on client-side

--- client/js/app.js
+++ client/js/app.js
@@ -1,14 +1,57 @@
 var nickname = "v1per14";
+var channel = "";
+var host = "http://localhost:8081"
+
 // nickname = prompt('Введите никнем');	
 
+
+
 $(document).ready(function () {
-	var ws = new WebSocket('ws://localhost:8081');
+	
 
-	ws.onopen = function () {
-		alert('connected');
-	}
+	$('ul.rooms-list li').on('click', function () {
+		$('ul.rooms-list li').removeClass('active');
+		
+		$current = $(this);
 
-	ws.onerror = function () {
-		alert('error');
-	}
+		$current.addClass('active');
+
+		$('#form_connect_to_room #channel_message').html('Вы выбрали канал <b>' + $current.text() + '</b>')
+		$('#form_connect_to_room #room_field').val($current.data('value'))
+		channel = $current.data('value');
+	});
+
+	$('#form_create_room').on('submit', function (e) {
+		e.preventDefault
+
+		$current = $(this);
+
+		data = $current.serialize();
+
+		$.post( host + "/room/create", data, function (data) {
+			//Must return json
+		})
+	});
+
+	$('#form_connect_to_room').on('submit', function (e) {
+		e.preventDefault
+
+		$current = $(this)
+
+		data = $current.serialize();
+
+		$.post( host + "/user/connect", data, function (data) {
+			//Must return json
+		})
+
+		// var ws = new WebSocket('ws://localhost:8081');
+
+		// ws.onopen = function () {
+		// 	alert('connected');
+		// }
+
+		// ws.onerror = function () {
+		// 	alert('error');
+		// }	
+	});
 })
